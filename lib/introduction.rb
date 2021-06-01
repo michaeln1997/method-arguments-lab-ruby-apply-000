4f58cbc624ddb399731d3e1140bf4d4d27ef7a15
Michael Nunes <michael.mundial@hotmail.com>
2021-06-01 18:44:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,11 +7,11 @@ introduction("Josh")
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in 
-  #{language}.js."
+  #{language}."
 end
 
 
-introduction_with_language("Dan", "Ember")
+introduction_with_language("Dan", "Ember.js")
 
 
   
